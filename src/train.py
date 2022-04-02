98fa95884176b663810824866c4e0afe9a1fbff3
Mohamed <mhmdrdwn88@gmail.com>
2022-04-02 04:24:25 +0200

    fix parameter bug in features_set

--- src/train.py
+++ src/train.py
@@ -86,7 +86,9 @@ def train_speed_data(num_epochs=10):
 
 
 def train_direction_data(num_epochs=10):
-    print('Building Data...')
+    print('Reading Data...')
+    train_data, test_data, scaler = read_raw_data()
+    hours_ahead = 1
     xtrain, xval, ytrain, yval = make_wind_direction_data(train_data, 
         gap=hours_ahead)
     xtest, ytest = make_wind_direction_data(test_data, train=False, 
@@ -98,41 +100,45 @@ def train_direction_data(num_epochs=10):
     because we ave cosine and sine direction"""
     input_size = [14, 14]
     output_size = 14
-    
+    hidden_size = 16
+    num_layers = 1
+
+
     print('Training LSTM...')
     #build LSTM and train
     lstm_model = LSTM(output_size, input_size, hidden_size, num_layers)
     lstm_model = lstm_model.to(device)
-    lstm_model = run_train(lstm_model, train_iter, val_iter, num_epochs=num_epochs)
+    lstm_model = run_train(lstm_model, train_iter, val_iter, num_epochs=num_epochs,
+            features_set=1)
     y_true, y_preds = run_test_direction(lstm_model, test_iter, scaler, 
-        output_sine=True)
+            features_set=1, output_sine=True)
 
     print('Error Metrics...')
     results(y_true, y_preds, feature_name = 'direction', plots=False)
 
     #build fusion data
     xtrain_temp, xval_temp, _, _ = make_ready_data(train_data, 
-        feature='temperature', gap=hours_ahead)
+            feature='temperature', gap=hours_ahead)
     xtest_temp, _ = make_ready_data(test_data, train=False, 
-        feature='temperature', gap=hours_ahead)
+            feature='temperature', gap=hours_ahead)
     xtrain, xval, ytrain, yval = make_wind_direction_data(train_data, 
-        gap=hours_ahead)
+            gap=hours_ahead)
     xtest, ytest = make_wind_direction_data(test_data, train=False, 
-        gap=hours_ahead)
+            gap=hours_ahead)
     train_iter, val_iter, test_iter, device = build_dataloader(xtrain, xval, 
-        xtest, ytrain, yval, ytest, xtrain_temp, xval_temp, xtest_temp, 
-        add_temp=True)
+            xtest, ytrain, yval, ytest, xtrain_temp, xval_temp, xtest_temp, 
+            add_temp=True)
     
     print('Training BiLinear LSTM...')
     #build LSTM with fusion and train
     input_size = [14, 7] # cosine+sine features = 14, tempretaure features = 7
     output_size = 14 # cosine+sine features = 14
     lstm_model = BiLinearPoolingLSTM(output_size, input_size, hidden_size, 
-        num_layers)
+            num_layers)
     lstm_model = lstm_model.to(device)
     lstm_model = run_train(lstm_model, train_iter, val_iter, num_epochs=num_epochs)
     y_true, y_preds = run_test_direction(lstm_model, test_iter, scaler, 
-        output_sine=True)
+            output_sine=True)
 
     print('Error Metrics...')
     results(y_true, y_preds, feature_name = 'direction', plots=False)
@@ -140,11 +146,11 @@ def train_direction_data(num_epochs=10):
     print('Training AutoLSTM...')
     #build AutoencoderLSTM and train
     model = BiLinearPoolingAutoEncoderLSTM(output_size, input_size, hidden_size, 
-        num_layers)
+            num_layers)
     model = model.to(device)
     model = run_train(model, train_iter, val_iter, num_epochs=num_epochs, outputs_nr=3)
     y_true, y_preds = run_test_direction(model, test_iter, scaler, outputs_nr=3, 
-        output_sine=True)
+            output_sine=True)
 
     print('Error Metrics...')
     results(y_true, y_preds, feature_name = 'direction', plots=False)
